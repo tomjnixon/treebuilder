2b2ab9894ce874b83ea3f945f2a335f7aff73ec3
Thomas Nixon <tom@tomn.co.uk>
2015-12-06 22:15:27 +0000

    Return cpp code with preview.

--- src/js_compile_handler.erl
+++ src/js_compile_handler.erl
@@ -15,7 +15,7 @@ handle(Req, State1) ->
     cowboy_utils:json_req_response(Req, State1,
       fun (#{<<"cpp_code">> := CppCode}, State) ->
               {JsCode, Errors} = js_compiler:compile_js(CppCode),
-              {ok, #{js_code => JsCode, errors => Errors}, State}
+              {ok, #{js_code => JsCode, cpp_code => CppCode, errors => Errors}, State}
       end).
 
 terminate(_Reason, _Req, _State) ->
